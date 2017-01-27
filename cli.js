01361fb1b9cd54bd0362754092bf7cbfa1155a3b
Thibaud Colas <thibaudcolas@gmail.com>
2017-01-27 17:02:18 +0200

    Introduce manual mode to allow interaction with the page before screenshotting

--- cli.js
+++ cli.js
@@ -10,7 +10,7 @@ const cli = meow(`
     Example
       $ screenshotron example.com
       ${logSymbols.success} OK
-      $ screenshotron example.com bananas.com --delay 3000
+      $ screenshotron example.com bananas.com --size 320x480 --delay 3000 --manual
       ${logSymbols.success} OK
 
     Options
@@ -18,6 +18,7 @@ const cli = meow(`
       --version,   Display the version number
       --delay [ms], Wait for a given period before taking a screenshot.
       --size [widthxheight], Configure screenshot size. Default: 1440x1200
+      --manual, Show the website to interact with it during the delay.
 `);
 
 if (cli.input.length === 0) {
@@ -28,8 +29,9 @@ if (cli.input.length === 0) {
 const urls = cli.input.map(parseURL);
 const size = cli.flags.size ? cli.flags.size : '1440x1200';
 const delay = cli.flags.delay ? Math.max(parseInt(cli.flags.delay, 10), 1) : 1;
+const manual = !!cli.flags.manual;
 
-takeScreenshot(urls, delay, size, (err, results) => {
+takeScreenshot(urls, delay, size, manual, (err, results) => {
     if (err) {
         console.log(`${logSymbols.warning} ${err}`);
         process.exit(1);
